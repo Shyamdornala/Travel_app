50832a73f8305da8354d8f684fd9e583c91950c2
Yeshwanth Dornala <yeshwanth.dornala@nirman.io>
2021-10-26 12:25:17 +0530

    initial commit

---
+++ components/Rating.js
@@ -0,0 +1,36 @@
+import React from "react";
+import {View, Image} from 'react-native'
+import { exp } from "react-native/Libraries/Animated/src/Easing";
+
+import { icons } from "../constants";
+
+const Rating = ({containerStyle, rate}) => {
+
+    const starComponents = []
+
+    for (var i = 0; i < rate; i++){
+        starComponents.push(
+            <Image
+                key={`full-${i}`}
+                source={icons.star}
+                resizeMode="cover"
+                style={{
+                    marginLeft: i == 0? 0 : 5,
+                    width : 15,
+                    height:15
+                }}
+            />
+
+        )
+    }
+    return(
+        <View style={{
+            flexDirection:'row',
+            ...containerStyle
+        }}>{starComponents}
+
+        </View>
+    )
+}
+
+export default Rating;
